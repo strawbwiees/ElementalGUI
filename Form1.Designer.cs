9ac6ad39b18dd8f8d75058f0e6fedc2d3ba955c1
noe <noellamarizbeltran01@gmail.com>
2026-09-20 16:53:40 +0800

    assets

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,7 +44,7 @@
             button1.TabIndex = 0;
             button1.Text = "PLAY";
             button1.UseVisualStyleBackColor = false;
-            button1.Click += play; 
+            button1.Click += play;
             // 
             // button2
             // 
